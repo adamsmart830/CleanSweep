740dc4756725c871d450d07946a281e47331f773
rspacerr <definitelynotoberon@gmail.com>
2024-02-09 16:44:07 -0500

    npm start dev script

--- clean-sweep/backend/server.js
+++ clean-sweep/backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PROT || 3500 // Change to whatever local port
+const PORT = process.env.PORT || 3500 // Change to whatever local port
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
