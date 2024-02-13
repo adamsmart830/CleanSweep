0c1ad0a70276d02c770ae5f6f50a34412b47494a
rspacerr <definitelynotoberon@gmail.com>
2024-02-13 17:18:44 -0500

    mongoDB connection

--- clean-sweep/backend/server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3500 // Change to whatever local port
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
