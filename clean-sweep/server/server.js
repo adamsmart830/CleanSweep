b69b4233d79c3b9544bd9e0eb226c7f650afa98b
rspacerr <definitelynotoberon@gmail.com>
2024-02-19 21:00:33 -0500

    store reports logic

--- clean-sweep/server/server.js
+++ clean-sweep/server/server.js
@@ -4,12 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 3500 // Change to whatever local port
 const mongoose = require('mongoose');
 
+// from dotenv 
 USER = process.env.MONGO_USER; 
 PASSWD = process.env.MONGO_PASSWORD;
 CLUSTER = process.env.MONGO_CLUSTER;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+// confirm connection to database 
 mongoose.connect(`mongodb+srv://${USER}:${PASSWD}@$${CLUSTER}.mongodb.net`)
 .then(() => {
     console.log("Successfully connected to MongoDB");
@@ -17,12 +19,16 @@ mongoose.connect(`mongodb+srv://${USER}:${PASSWD}@$${CLUSTER}.mongodb.net`)
     console.log("Error connecting to MongoDB:", err);
 });
 
-// endpoints for reports
-app.post("/report",async(req,res) =>{
-    try{
+const Report = require("./models/reports");
 
-    } catch(error){
-        console.log("Error submitting report!", error);
+// endpoints for reports
+app.post("/report", async(req,res) =>{
+    try{
+        const {type, location} = req.body;
+        const newReport = new Report({type, location});
+        await newReport.save();
+    } catch(err){
+        console.log("Error submitting report!", err);
         res.status(500).json({message:"Report submission failed."});
     }
 })
