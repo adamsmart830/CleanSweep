6a8bf66bbcbcbdb0a05ead7d20afe0380b5adc8f
unknown <iamchwill@example.com>
2024-02-23 17:07:24 -0500

    Changed cluster, but still not functioning. Will swap to attempting to use Mongoose

--- clean-sweep/server/db/conn.js
+++ clean-sweep/server/db/conn.js
@@ -1,30 +1,25 @@
-const { MongoClient, ServerApiVersion  } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
- 
-var _db;
- 
+
+let _db;
+
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      console.log("Working");
-      if (db)
-      {
-        _db = db.db("cleansweep");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      console.log("Finished");
-      return callback(err);
-         });
+  connectToServer: async function (callback) {
+
+    try {
+      await client.connect();
+    } catch (e) {
+      console.error(e);
+    }
+
+    _db = client.db("employees");
+
+    return (_db === undefined ? false : true);
   },
- 
   getDb: function () {
     return _db;
   },
--- clean-sweep/server/server.js
+++ clean-sweep/server/server.js
@@ -8,10 +8,11 @@ app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
-app.listen(port, () => {
+
+app.listen(port, async () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
+  await dbo.connectToServer(function (err) {
     if (err) console.error(err);
-   });
+  });
   console.log(`Server is running on port: ${port}`);
 });
