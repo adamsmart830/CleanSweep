ded8a075306c9119ce7905d77ac5119dd437d954
cstecc <46802772+cstecc@users.noreply.github.com>
2024-02-25 13:23:43 -0800

    Update App.js
    go to loginpage directly

--- clean-sweep/App.js
+++ clean-sweep/App.js
@@ -1,10 +1,11 @@
 // import { StyleSheet, Text, View } from 'react-native'; at the moment dont need any of these imports from react-native
 import EventPage from './pages/EventPage'; //the page we are using
-
+import LoginPage from './pages/LoginPage'; //the page we are using
 export default function App() {
   return (
   //  <View style={styles.container}> dont need styling for now
-      <EventPage /> //goes to the page
+      //<EventPage /> //goes to the page
+      <LoginPage /> //goes to the page
   //  </View> dont need styling for now
   );
 }
