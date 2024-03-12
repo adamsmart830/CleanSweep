fb2f3264824209eefe52262179a66855fb5887ad
hzmann <hzitzmann@optonline.net>
2024-03-12 16:54:56 -0400

    Established LOGIN project to hold user login information with client and server sides. Signup.jsx in client side stores the frontend design for login page, including Name, Email, and Password fields, a Register button, and then a Login button for users who already have an account

---
+++ clean-sweep/LOGIN/client/src/Signup.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function Signup(){
+
+    return (
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Register</h2>
+                <form>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Name</strong>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Name"
+                        autoComplete="off"
+                        name="email"
+                        className="form-control rounded-0"
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Email</strong>
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Enter Email"
+                            autoComplete="off"
+                            name="email"
+                            className="form-control rounded-0"
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlfor="email">
+                            <strong>Password</strong>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            className="form-control rounded-0"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        Register
+                    </button>
+                    <p>Already Have an Account</p>
+                    <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
