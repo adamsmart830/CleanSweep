8b1f5828175199c1cd2d18f3808dda3d3fb196d4
unknown <iamchwill@example.com>
2024-03-12 16:24:56 -0400

    minor changes to testDB that doesn't work

--- clean-sweep/server/server.js
+++ clean-sweep/server/server.js
@@ -1,17 +1,8 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-   });
-  console.log(`Server is running on port: ${port}`);
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://chenw22:NNnAQCNm0fHATMLR@cluster0.4klc5mn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
 });
